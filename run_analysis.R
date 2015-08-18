5f44ed740d7c468f6e7d206c4bca1cfb0b341df6
Kentryo <kenhuang@udel.edu>
2015-08-18 16:55:15 -0400

    Load raw data

--- run_analysis.R
+++ run_analysis.R
@@ -2,10 +2,25 @@
 # clear workspace
 rm(list = ls())
 #load necessary R packages
-if(!curl %in% installed.packages()){install.packages("curl")}
+
 #Download the data file and save them in get and clean data folder
 if(!file.exists("./cleandata_project")){dir.creat("./cleandata_project")}
 fileurl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-download.file(fileurl, destfile = "./cleandata_project/rawdata.zip", method = "curl")
+download.file(fileurl, destfile = "./cleandata_project/rawdata.zip")
 #unzip the file
 unzip(zipfile = "./cleandata_project/rawdata.zip", exdir = "./cleandata_project")
+
+#Load the raw data file
+data_path <- "./cleandata_project/UCI HAR Dataset"
+#Read Activity files
+ActivityTest <- read.table(file.path(data_path, "test", "Y_test.txt"))
+ActivityTrain <- read.table(file.path(data_path, "train", "Y_train.txt"))
+#Read Subject files
+SubjectTest <- read.table(file.path(data_path, "test", "subject_test.txt"))
+SubjectTrain <- read.table(file.path(data_path, "train", "subject_train.txt"))
+#Read Feature files
+FeatureTest <- read.table(file.path(data_path, "test", "X_test.txt"))
+FeatureTrain <- read.table(file.path(data_path, "train", "X_train.txt"))
+#Read Name and level files
+FeatureName <- read.table(file.path(data_path, "features.txt"))
+ActivityLabel <- read.table(file.path(data_path, "activity_lables.txt"))
